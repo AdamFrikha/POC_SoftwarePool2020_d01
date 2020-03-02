78e751f821f4fb546f8d304ad0a200b1ea7bd984
A_FRIKHA <A_FRIKHA@localhost.localdomain>
2020-03-02 08:02:56 -0500

    [ADD] cookies and header

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const headerParser = require('header-parser');
+const cookieParser = require('cookie-parser');
 
 function startServer() {
   const app = express();
   app.use(bodyParser.text());
+  app.use(headerParser);
+  app.use(cookieParser);
   app.get('/hello', (req, res) => {
     res.send('world');
   });
@@ -19,6 +23,18 @@ function startServer() {
   app.post('/repeat-my-body', (req, res) => {
     if(req.body)
       res.send(req.body);
+    else
+      res.sendStatus(400);
+  });
+  app.get('/repeat-my-header', (req, res) => {
+    if(req.header.message)
+      res.send(req.header.message);
+    else
+      res.sendStatus(400);
+  });
+  app.get('/repeat-my-cookie', (req, res) => {
+    if(req.cookies.message)
+      res.send(req.cookies.message);
     else
       res.sendStatus(400);
   });
