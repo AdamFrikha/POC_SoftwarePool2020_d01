a0b5d14623185c8533a77a684d48a6ff922fd775
A_FRIKHA <A_FRIKHA@localhost.localdomain>
2020-03-02 05:09:08 -0500

    [ADD] request hello, with reponse world

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function startServer() {
+  const express = require('express');
+  const app = express();
+  app.get('/hello', (req, res) => {
+    res.send('world');
+  });
+  app.listen(8080);
+}
+
+startServer();
